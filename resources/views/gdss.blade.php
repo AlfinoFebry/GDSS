b14cafcada4d6d3431ae4f12c96153fce3a30333
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2023-12-21 15:45:03 +0700

    .

--- resources/views/gdss.blade.php
+++ resources/views/gdss.blade.php
@@ -6,7 +6,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>SPK dengan Metode GDSS Borda</h1>
+                        <h1>SPK dengan Metode GDSS</h1>
                     </div>
                 </div>
             </div><!-- /.container-fluid -->
@@ -14,8 +14,112 @@
 
         <!-- Main content -->
         <section class="content">
+            <!-- Default box -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Rank Mabac</h3>
 
-            
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <tr>
+                                    <th>Rank</th>
+                                    <th>Alternative</th>
+                                </tr>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- @foreach
+                                <tr>
+                                    <th></th>
+                                </tr>
+                            @endforeach --}}
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+
+            </div>
+            <!-- /.card -->
+
+            <!-- Default box -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Rank Electre</h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <tr>
+                                    <th>Rank</th>
+                                    <th>Alternative</th>
+                                </tr>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- @foreach
+                                <tr>
+                                    <th></th>
+                                </tr>
+                            @endforeach --}}
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+
+            </div>
+            <!-- /.card -->
+
+            <!-- Default box -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Rank GDSS</h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <tr>
+                                    <th>Rank</th>
+                                    <th>Alternative</th>
+                                </tr>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- @foreach
+                                <tr>
+                                    <th></th>
+                                </tr>
+                            @endforeach --}}
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+
+            </div>
             <!-- /.card -->
         </section>
         <!-- /.content -->
